a12137192c8afe716a0d177c7ef1783a4129d502
mrfrk <mrfrkayvaz@gmail.com>
2025-08-15 01:03:43 +0300

    initial commit with nextra

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  turbopack: {
+    resolveAlias: {
+      'next-mdx-import-source-file': './src/mdx-components.js'
+    }
+  }
+})
